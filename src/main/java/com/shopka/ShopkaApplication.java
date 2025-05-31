2e030e6f25606b5932c64cb9190b27389bf3884b
MariuszKam <rexxarhell@gmail.com>
2025-05-31 22:33:04 +0200

    init commit

---
+++ src/main/java/com/shopka/ShopkaApplication.java
@@ -0,0 +1,13 @@
+package com.shopka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopka/ShopkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
