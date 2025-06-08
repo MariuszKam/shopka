886dbfceb1e06d982912c25f8f13ff4105255763
MariuszKam <87607813+MariuszKam@users.noreply.github.com.>
2025-06-08 19:17:45 +0200

    Adapter for Role Repository

--- src/main/java/com/shopka/infrastracture/user/role/JpaRoleRepository.java
+++ src/main/java/com/shopka/infrastracture/user/role/JpaRoleRepository.java
@@ -7,28 +7,30 @@ import com.shopka.domain.user.role.RoleId;
 import com.shopka.domain.user.role.RoleName;
 import com.shopka.domain.user.role.repository.RoleRepository;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@AllArgsConstructor
 public class JpaRoleRepository implements RoleRepository {
+
+    private final JpaRoleEntityRepository jpaRoleEntityRepository;
+
+    private final RoleMapper roleMapper;
 
     @Override
     public Optional<Role> findById(RoleId id) {
-        // TODO Auto-generated method stub
-        return Optional.empty();
+        return jpaRoleEntityRepository.findById(id.getValue()).map(roleMapper::toRole);
     }
 
     @Override
     public Optional<Role> findByName(RoleName name) {
-        // TODO Auto-generated method stub
-        return Optional.empty();
+        return jpaRoleEntityRepository.findByName(name.getName()).map(roleMapper::toRole);
     }
 
     @Override
     public void save(Role role) {
-        // TODO Auto-generated method stub
-        
+        jpaRoleEntityRepository.save(roleMapper.toRoleEntity(role));
     }
 
-    
 }
